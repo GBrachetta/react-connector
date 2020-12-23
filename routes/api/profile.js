1e530dd0538df8ee5b3decab4265280142185f89
Guillermo Brachetta <brachetta@me.com>
2020-12-23 16:20:39 +0100

    Add work experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -106,7 +106,6 @@ router.post(
 // @route   GET api/profile
 // @desc    Get all profiles
 // @access  Public
-
 router.get('/', async (req, res) => {
   try {
     const profiles = await Profile.find().populate('user', ['name', 'avatar']);
@@ -120,7 +119,6 @@ router.get('/', async (req, res) => {
 // @route   GET api/profile/user/:user_id
 // @desc    Get one user profile by user ID
 // @access  Public
-
 router.get('/user/:user_id', async (req, res) => {
   try {
     const profile = await Profile.findOne({
@@ -142,10 +140,10 @@ router.get('/user/:user_id', async (req, res) => {
 // @route   DELETE api/profile
 // @desc    Delete profile, user and posts
 // @access  Private
-
 router.delete('/', auth, async (req, res) => {
   try {
     // @todo - remove users posts
+    // @
 
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
@@ -157,5 +155,58 @@ router.delete('/', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('company', 'Company is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
