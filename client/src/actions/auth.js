d0b8308f4e887bed4b549bbdb84b39338d7bbd39
Guillermo Brachetta <brachetta@me.com>
2020-12-24 16:03:36 +0100

    Create Profile component. Create spinner. Dashboard setup

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,7 +8,8 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGOUT
+  LOGOUT,
+  CLEAR_PROFILE
 } from './types';
 
 // load user
@@ -95,5 +96,6 @@ export const login = (email, password) => async (dispatch) => {
 
 // Logout & clear Profile
 export const logout = () => (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
